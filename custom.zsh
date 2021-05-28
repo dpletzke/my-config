120b995d847cb43c6e61c1c0c0265a9c1fb0f66a
daniel <dpletzke@gmail.com>
2021-05-28 15:16:37 -0500

    add zsh custom prompt and else config

---
+++ custom.zsh
@@ -0,0 +1,20 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+alias cl="clear"
+
+alias zshconfig="st ~/.zshrc"
+alias zshenv="st ~/.zshenv"
+alias ohmyalias="st ~/.oh-my-zsh/custom/custom.zsh"
+alias ohmyzsh="st ~/.oh-my-zsh"
+alias sourcezsh="source ~/.oh-my-zsh/custom/custom.zsh && source ~/.zshrc"
+
+alias vin="vagrant up && vagrant ssh"
